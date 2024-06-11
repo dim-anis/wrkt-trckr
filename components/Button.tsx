7749598b7e19de82919a301efaeb248e25f96343
dim-anis <anis.dim@gmail.com>
2024-06-11 16:15:52 +0700

    add button component

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,4 +1,5 @@
 import { Theme } from '@/lib/theme';
+import { Text } from './Text';
 import {
   createVariant,
   createRestyleComponent,
@@ -12,31 +13,46 @@ import {
   ColorProps,
   BackgroundColorProps,
   LayoutProps,
-  BorderProps
+  BorderProps,
+  useRestyle,
+  spacing,
+  SpacingProps,
+  composeRestyleFunctions
 } from '@shopify/restyle';
-import { Button as DefaultButton } from 'react-native';
+import { Button as DefaultButton, Pressable } from 'react-native';
+import { Box } from './Box';
 
-const Button = createRestyleComponent<
-  VariantProps<Theme, 'buttonVariants'> &
-    ColorProps<Theme> &
-    BackgroundColorProps<Theme> &
-    TypographyProps<Theme> &
-    LayoutProps<Theme> &
-    BorderProps<Theme> &
-    React.ComponentProps<typeof DefaultButton>,
-  Theme
->(
-  [
-    color,
-    backgroundColor,
-    typography,
-    layout,
-    border,
-    createVariant<Theme, 'buttonVariants'>({
-      themeKey: 'buttonVariants'
-    })
-  ],
-  DefaultButton
-);
+type RestyleProps = SpacingProps<Theme> &
+  ColorProps<Theme> &
+  BorderProps<Theme> &
+  BackgroundColorProps<Theme> &
+  VariantProps<Theme, 'buttonVariants'>;
 
-export default Button;
+const restyleFunctions = composeRestyleFunctions<Theme, RestyleProps>([
+  color,
+  spacing,
+  border,
+  backgroundColor,
+  createVariant<Theme, 'buttonVariants'>({
+    themeKey: 'buttonVariants'
+  })
+]);
+
+type ButtonProps = RestyleProps & {
+  label: string;
+  onPress: () => void;
+};
+
+export default function Button({ onPress, label, ...rest }: ButtonProps) {
+  const props = useRestyle(restyleFunctions, rest);
+
+  return (
+    <Pressable onPress={onPress}>
+      <Box {...props}>
+        <Text variant="buttonLabel" labelVariant={rest.variant}>
+          {label}
+        </Text>
+      </Box>
+    </Pressable>
+  );
+}
