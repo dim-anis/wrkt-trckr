285287253e30ee1502dd7d8be55e8c743d3a6815
dim-anis <anis.dim@gmail.com>
2024-06-11 16:14:50 +0700

    extend Text component with restyle

--- components/Text.tsx
+++ components/Text.tsx
@@ -1,4 +1,55 @@
 import { Theme } from '@/lib/theme';
-import { createText } from '@shopify/restyle';
+import {
+  ColorProps,
+  LayoutProps,
+  OpacityProps,
+  SpacingProps,
+  TextShadowProps,
+  TypographyProps,
+  VariantProps,
+  VisibleProps,
+  color,
+  createRestyleComponent,
+  createText,
+  createVariant,
+  layout,
+  opacity,
+  spacing,
+  textShadow,
+  typography,
+  visible
+} from '@shopify/restyle';
+import React from 'react';
+import { Text as DefaultText } from 'react-native';
 
-export const Text = createText<Theme>();
+export const Text = createRestyleComponent<
+  ColorProps<Theme> &
+    OpacityProps<Theme> &
+    VisibleProps<Theme> &
+    TypographyProps<Theme> &
+    TextShadowProps<Theme> &
+    SpacingProps<Theme> &
+    LayoutProps<Theme> &
+    VariantProps<Theme, 'textVariants'> &
+    VariantProps<Theme, 'buttonLabelVariants', 'labelVariant'> &
+    React.ComponentProps<typeof DefaultText>,
+  Theme
+>(
+  [
+    createVariant<Theme, 'buttonLabelVariants', 'labelVariant'>({
+      themeKey: 'buttonLabelVariants',
+      property: 'labelVariant'
+    }),
+    createVariant<Theme, 'textVariants'>({
+      themeKey: 'textVariants'
+    }),
+    color,
+    opacity,
+    visible,
+    typography,
+    textShadow,
+    spacing,
+    layout
+  ],
+  DefaultText
+);
