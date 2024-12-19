08cedbdbf483a5348cc5ad07b56eb084ebebe0b4
dim-anis <anis.dim@gmail.com>
2024-12-19 17:28:38 +0700

    refactor: improve performance

--- components/ui/Skeleton.tsx
+++ components/ui/Skeleton.tsx
@@ -4,9 +4,10 @@ import Animated, {
   withRepeat,
   withTiming
 } from 'react-native-reanimated';
-import { ComponentProps, useEffect } from 'react';
+import { ComponentProps, useEffect, useMemo } from 'react';
 import { useTheme } from '@shopify/restyle';
 import { Theme } from '@/lib/theme';
+import { Box } from './Box';
 
 export default function Skeleton({
   style
@@ -14,21 +15,202 @@ export default function Skeleton({
   const opacity = useSharedValue(1);
   const theme = useTheme<Theme>();
 
+  const animationConfig = useMemo(
+    () => ({
+      opacity: withRepeat(
+        withTiming(0.5, { duration: 1000 }),
+        -1, // Repeat infinitely
+        true // Reverse the animation (0.5 -> 1)
+      )
+    }),
+    []
+  );
+
   useEffect(() => {
-    opacity.value = withRepeat(
-      withTiming(0.5, { duration: 1000 }),
-      -1, // Repeat infinitely
-      true // Reverse the animation (0.5 -> 1)
-    );
-  }, [opacity]);
+    opacity.value = animationConfig.opacity;
+  }, [animationConfig]);
 
-  const animatedStyle = useAnimatedStyle(() => {
-    return {
-      opacity: opacity.value,
-      backgroundColor: theme.colors.secondary,
-      borderRadius: 6
-    };
-  });
+  const animatedStyle = useAnimatedStyle(() => ({
+    opacity: opacity.value,
+    backgroundColor: theme.colors.secondary,
+    borderRadius: 6
+  }));
 
-  return <Animated.View style={[animatedStyle, style]} />;
+  // return <Animated.View style={[animatedStyle, style]} />;
+
+  return (
+    <Box padding="m" gap="xl">
+      <Box gap="m">
+        <Box gap="m">
+          <Animated.View style={[animatedStyle, { height: 40 }]} />
+          <Box gap="m">
+            <Animated.View style={[animatedStyle, { height: 20 }]} />
+            <Animated.View style={[animatedStyle, { height: 40 }]} />
+          </Box>
+        </Box>
+        <Box gap="m">
+          <Box gap="s" flexDirection="row">
+            <Animated.View
+              style={[animatedStyle, { height: 40, width: 40, flex: 1 }]}
+            />
+            <Animated.View style={[animatedStyle, { height: 40, flex: 2 }]} />
+            <Animated.View style={[animatedStyle, { height: 40, flex: 2 }]} />
+            <Animated.View style={[animatedStyle, { height: 40, flex: 2 }]} />
+            <Animated.View
+              style={{
+                height: 40,
+                width: 40,
+                flex: 1
+              }}
+            />
+          </Box>
+          <Box gap="s" flexDirection="row">
+            <Animated.View
+              style={[animatedStyle, { height: 40, width: 40, flex: 1 }]}
+            />
+            <Animated.View style={[animatedStyle, { height: 40, flex: 2 }]} />
+            <Animated.View style={[animatedStyle, { height: 40, flex: 2 }]} />
+            <Animated.View style={[animatedStyle, { height: 40, flex: 2 }]} />
+            <Animated.View
+              style={{
+                height: 40,
+                width: 40,
+                flex: 1
+              }}
+            />
+          </Box>
+          <Box gap="s" flexDirection="row">
+            <Animated.View
+              style={[animatedStyle, { height: 40, width: 40, flex: 1 }]}
+            />
+            <Animated.View style={[animatedStyle, { height: 40, flex: 2 }]} />
+            <Animated.View style={[animatedStyle, { height: 40, flex: 2 }]} />
+            <Animated.View style={[animatedStyle, { height: 40, flex: 2 }]} />
+            <Animated.View
+              style={{
+                height: 40,
+                width: 40,
+                flex: 1
+              }}
+            />
+          </Box>
+        </Box>
+        <Animated.View style={[animatedStyle, { height: 40 }]} />
+      </Box>
+      <Box gap="m">
+        <Box gap="m">
+          <Animated.View style={[animatedStyle, { height: 40 }]} />
+          <Box gap="m">
+            <Animated.View style={[animatedStyle, { height: 20 }]} />
+            <Animated.View style={[animatedStyle, { height: 40 }]} />
+          </Box>
+        </Box>
+        <Box gap="m">
+          <Box gap="s" flexDirection="row">
+            <Animated.View
+              style={[animatedStyle, { height: 40, width: 40, flex: 1 }]}
+            />
+            <Animated.View style={[animatedStyle, { height: 40, flex: 2 }]} />
+            <Animated.View style={[animatedStyle, { height: 40, flex: 2 }]} />
+            <Animated.View style={[animatedStyle, { height: 40, flex: 2 }]} />
+            <Animated.View
+              style={{
+                height: 40,
+                width: 40,
+                flex: 1
+              }}
+            />
+          </Box>
+          <Box gap="s" flexDirection="row">
+            <Animated.View
+              style={[animatedStyle, { height: 40, width: 40, flex: 1 }]}
+            />
+            <Animated.View style={[animatedStyle, { height: 40, flex: 2 }]} />
+            <Animated.View style={[animatedStyle, { height: 40, flex: 2 }]} />
+            <Animated.View style={[animatedStyle, { height: 40, flex: 2 }]} />
+            <Animated.View
+              style={{
+                height: 40,
+                width: 40,
+                flex: 1
+              }}
+            />
+          </Box>
+          <Box gap="s" flexDirection="row">
+            <Animated.View
+              style={[animatedStyle, { height: 40, width: 40, flex: 1 }]}
+            />
+            <Animated.View style={[animatedStyle, { height: 40, flex: 2 }]} />
+            <Animated.View style={[animatedStyle, { height: 40, flex: 2 }]} />
+            <Animated.View style={[animatedStyle, { height: 40, flex: 2 }]} />
+            <Animated.View
+              style={{
+                height: 40,
+                width: 40,
+                flex: 1
+              }}
+            />
+          </Box>
+        </Box>
+        <Animated.View style={[animatedStyle, { height: 40 }]} />
+      </Box>
+      <Box gap="m">
+        <Box gap="m">
+          <Animated.View style={[animatedStyle, { height: 40 }]} />
+          <Box gap="m">
+            <Animated.View style={[animatedStyle, { height: 20 }]} />
+            <Animated.View style={[animatedStyle, { height: 40 }]} />
+          </Box>
+        </Box>
+        <Box gap="m">
+          <Box gap="s" flexDirection="row">
+            <Animated.View
+              style={[animatedStyle, { height: 40, width: 40, flex: 1 }]}
+            />
+            <Animated.View style={[animatedStyle, { height: 40, flex: 2 }]} />
+            <Animated.View style={[animatedStyle, { height: 40, flex: 2 }]} />
+            <Animated.View style={[animatedStyle, { height: 40, flex: 2 }]} />
+            <Animated.View
+              style={{
+                height: 40,
+                width: 40,
+                flex: 1
+              }}
+            />
+          </Box>
+          <Box gap="s" flexDirection="row">
+            <Animated.View
+              style={[animatedStyle, { height: 40, width: 40, flex: 1 }]}
+            />
+            <Animated.View style={[animatedStyle, { height: 40, flex: 2 }]} />
+            <Animated.View style={[animatedStyle, { height: 40, flex: 2 }]} />
+            <Animated.View style={[animatedStyle, { height: 40, flex: 2 }]} />
+            <Animated.View
+              style={{
+                height: 40,
+                width: 40,
+                flex: 1
+              }}
+            />
+          </Box>
+          <Box gap="s" flexDirection="row">
+            <Animated.View
+              style={[animatedStyle, { height: 40, width: 40, flex: 1 }]}
+            />
+            <Animated.View style={[animatedStyle, { height: 40, flex: 2 }]} />
+            <Animated.View style={[animatedStyle, { height: 40, flex: 2 }]} />
+            <Animated.View style={[animatedStyle, { height: 40, flex: 2 }]} />
+            <Animated.View
+              style={{
+                height: 40,
+                width: 40,
+                flex: 1
+              }}
+            />
+          </Box>
+        </Box>
+        <Animated.View style={[animatedStyle, { height: 40 }]} />
+      </Box>
+    </Box>
+  );
 }
